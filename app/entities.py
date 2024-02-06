0b5c95088ec59fcdd920522943d4aaaea61cc3de
Aprendiz <Aprendiz@BOGDFPCGMF4148.FormacionSena.RED>
2024-02-06 17:20:13 -0500

    primeras entidades

---
+++ app/entities.py
@@ -0,0 +1,9 @@
+
+class Carro():
+    placa = "SDF 456"
+    tipo_vehiculos = "camion"
+    
+class Cliente():
+    nombre = "Yeritson Ribas"
+    celular = 3117855632
+    identificacion = 3117855632
